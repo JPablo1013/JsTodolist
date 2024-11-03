b3fe7cadc7e40a35895b8345b3723337dada495f
Juan Pablo Garcia Vega <18031402@itcelaya.edu.mx>
2024-11-02 18:58:01 -0600

    jnode

--- js/index.js
+++ js/index.js
@@ -1,45 +1,11 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const title = document.getElementById('title');
-  const description = document.getElementById('description');
-  const table = document.getElementById('table');
-  const alert = document.getElementById('alert');
-  const btn = document.getElementById('add');
-  let id = 1;
+import Model from './model.js';
+import View from './view.js';
 
-  function removeTodo(id) {
-    document.getElementById(id).remove();
-}
+document.addEventListener('DOMContentLoaded', () => {
+  const model = new Model();
+  const view = new View();
+  model.setView(view);
+  view.setModel(model);
 
-function addTodo() {
-    if (title.value === '' || description.value === '') {
-        alert.classList.remove('d-none');
-        alert.innerText = 'Title and description are required';
-        return;
-    }
-
-    alert.classList.add('d-none');
-    const row = table.insertRow();
-    row.setAttribute('id', id++);
-    row.innerHTML = `
-        <td>${title.value}</td>
-        <td>${description.value}</td>
-        <td class="text-center">
-            <input type="checkbox">
-        </td>
-        <td class="text-right">
-            <button class="btn btn-primary mb-1">
-                <i class="fa fa-pencil"></i>
-            </button>
-        </td>
-    `;
-    
-    const removeBtn = document.createElement('button');
-    removeBtn.classList.add('btn', 'btn-danger', 'mb-1', 'ml-1');
-    removeBtn.innerHTML = '<i class="fa fa-trash"></i>';
-    removeBtn.onclick = function (e) {
-        removeTodo(row.getAttribute('id'));
-    }
-    row.children[3].appendChild(removeBtn);
-  }
-  btn.onclick = addTodo;
+  view.render();
 });
